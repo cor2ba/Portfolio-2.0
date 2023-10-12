7aedf6ea8797c8799ecfe30df24299540a37ec21
Gabriel Córdoba <cordobagabrielignacio@gmail.com>
2023-10-12 04:25:04 -0300

    Different about me

--- src/components/modals/AboutMeModal.tsx
+++ src/components/modals/AboutMeModal.tsx
@@ -1,7 +1,7 @@
 import Exit from "@/assests/images/exit.png"
 import Image from "next/image"
 import { Modal } from "@/types/interfaces"
-import Me from "@/assests/images/me2.png"
+import Me from "@/assests/images/me.png"
 
 export default function AboutMeModal ({ modalAboutMe, setAboutMe }: Modal) {
     return (
@@ -9,11 +9,13 @@ export default function AboutMeModal ({ modalAboutMe, setAboutMe }: Modal) {
             <Image src={Me} alt="Image"></Image>
             <div className="flex flex-col justify-center items-start text-center mt-3 mr-3 gap-5 [&>p]:text-white [&>p]:text-start [&>p]:text-xl [&>p]:s:text-lg [&>p]:font-mono">
                 <p>I graduated of Full-Stack developer web in Henry Bootcamp 💻</p>
-                <p>I worked as an assistant professor at Henry 💛</p>
-                <p>I worked as an Front End Developer at CrESI 💜</p>
-                <p>Actually working at Neuron Health as Front End Developer 💙</p>
-                <p>I live in Argentina, Buenos Aires 🤍</p>
                 <p>Studying programming for more than 3 years 🔱</p>
+                <p>I live in Argentina, Buenos Aires 🤍</p>
+                <p>Passionate about the front end 🤎</p>
+                <p>Autodidact, creative, ingenius and enthusiastic 💫</p>
+                <p>I worked as an assistant professor at HENRY 💛</p>
+                <p>I worked as an Front End Developer at CRESI 💜</p>
+                <p>Actually working at NEURON HEALTH as Front End Developer 💙</p>
             </div>
             <Image onClick={() => setAboutMe(false)} className="absolute right-0 top-0 p-2 cursor-pointer" src={Exit} alt="Image"></Image>
         </div>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <title>Gabriel Cór2ba</title>
+      <title>Gabriel Córdoba</title>
       </Head>
       <body>
         <Main />
